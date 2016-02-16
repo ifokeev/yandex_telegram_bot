04882dd8683a0cd49a11ce3f94c9b9d16ab6667f
Ivan Fokeev <vanya@vanya.cc>
2016-02-16 20:21:36 +0300

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_options = { from: "robot@monolama.ru"  }
+  config.action_mailer.default_url_options = { :host => 'monolama.ru'  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port      => 587,
+    :address   => "smtp.mandrillapp.com",
+    :user_name => "",
+    :password  => ""
+
+  }
 end
